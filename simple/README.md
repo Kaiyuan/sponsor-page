a51103775cec235e3373bdd975a133efc9003b63
Kaiyuan <sealour@hotmail.com>
2016-10-02 06:21:39 +0800

    PayPal 改为收款页链接
    Paypal.me 是现在新的收款页面

--- simple/README.md
+++ simple/README.md
@@ -4,11 +4,11 @@
 
 #### 直接 Fork 之后需要修改以下内容为你的账户
 
-  /script.js:4-6 对应账户的二维码路径
+	/script.js:4-6 对应账户的二维码路径
 
-  /script.js:8  `var ppbusiness	=	"你的 PayPal 账号";  //建议用Unicode`
+	/index.html:28 PayPal.me 改为现在收款页面，这样可以删除原来的按钮方式了。
 
-  /index.html:37 `<input id="btc-key" type="text" value="比特币账号" readonly="readonly">`
+	/index.html:37 `<input id="btc-key" type="text" value="比特币账号" readonly="readonly">`
 
 #### 针对不同项目可以直接在 URL 加入项目参数和金额，不过仅仅作用于 PayPal 方式.
 
@@ -18,7 +18,7 @@
 #### 使用 `iframe` 嵌入页面的代码，高度至少 `240px`，宽度至少 `310px`！
 
 ```
-<iframe src="https://kaiyuan.github.io/donate-page/simple/?item=easy-select-style" style="overflow-x:hidden;overflow-y:hidden; border:0xp none #fff; min-height:240px; width:100%;"  frameborder="0" scrolling="no"></iframe>
+<iframe src="https://kaiyuan.github.io/donate-page/simple/" style="overflow-x:hidden;overflow-y:hidden; border:0xp none #fff; min-height:240px; width:100%;"  frameborder="0" scrolling="no"></iframe>
 ```
 
 ### License
--- simple/script.js
+++ simple/script.js
@@ -4,130 +4,8 @@ jQuery(document).ready(function() {
 	var BTCQR	=	'images/BTCQR.png';	// 二维码路径
 	var AliPayQR	=	'images/AliPayQR.png';
 	var WeChanQR	=	'images/WeChanQR.png';
-	// PayPal 账户建议使用 Unicode
-	var ppbusiness	=	"\u0073\u0065\u0061\u006c\u006f\u0075\u0072\u0040\u0067\u006d\u0061\u0069\u006c\u002e\u0063\u006f\u006d";
 
-	if (!ppitem_name) { ppitem_name	=	"Donate"; }
-
-	// if (!donatrNo) { donatrNo	=	1; }
-
-	//创建 Paypal 表单
-
-	// 捐赠方式，仅适用于国外账户!
-	function ppDonate() {
-		var tempForm	=	document.createElement("form");
-		tempForm.id	=	"paypal";
-		tempForm.method	=	"post";
-		tempForm.action	=	"https://www.paypal.com/cgi-bin/webscr";
-		tempForm.target	=	"paypal";
-
-		var cmd	=	document.createElement("input");
-		cmd.type	=	"hidden";
-		cmd.name	=	"cmd"
-		cmd.value	=	"_donations";
-		tempForm.appendChild(cmd);
-
-		var business	=	document.createElement("input");
-		business.type	=	"hidden";
-		business.name	=	"business"
-		business.value	=	ppbusiness;
-		tempForm.appendChild(business);
-
-		var lc	=	document.createElement("input");
-		lc.type	=	"hidden";
-		lc.name	=	"lc"
-		lc.value	=	"US";
-		tempForm.appendChild(lc);
-
-		var item_name	=	document.createElement("input");
-		item_name.type	=	"hidden";
-		item_name.name	=	"item_name"
-		item_name.value	=	ppitem_name;
-		tempForm.appendChild(item_name);
-
-		var no_note	=	document.createElement("input");
-		no_note.type	=	"hidden";
-		no_note.name	=	"no_note"
-		no_note.value	=	"0";
-		tempForm.appendChild(no_note);
-
-		var currency_code	=	document.createElement("input");
-		currency_code.type	=	"hidden";
-		currency_code.name	=	"currency_code"
-		currency_code.value	=	"USD";
-		tempForm.appendChild(currency_code);
-
-		var bn	=	document.createElement("input");
-		bn.type	=	"hidden";
-		bn.name	=	"bn"
-		bn.value	=	"PP-DonationsBF:btn_donate_SM.gif:NonHostedGuest";
-		tempForm.appendChild(bn);
-
-		document.body.appendChild(tempForm);
-		tempForm.submit();
-		document.body.removeChild(tempForm);
-	}
-
-	// 立即购买，适用于中国账户
-	function ppBuy() {
-		var tempForm	=	document.createElement("form");
-		tempForm.id	=	"paypal";
-		tempForm.method	=	"post";
-		tempForm.action	=	"https://www.paypal.com/cgi-bin/webscr";
-		tempForm.target	=	"paypal";
-
-		var cmd	=	document.createElement("input");
-		cmd.type	=	"hidden";
-		cmd.name	=	"cmd"
-		cmd.value	=	"_xclick";
-		tempForm.appendChild(cmd);
-
-		var business	=	document.createElement("input");
-		business.type	=	"hidden";
-		business.name	=	"business"
-		business.value	=	ppbusiness;
-		tempForm.appendChild(business);
-
-		var lc	=	document.createElement("input");
-		lc.type	=	"hidden";
-		lc.name	=	"lc"
-		lc.value	=	"US";
-		tempForm.appendChild(lc);
-
-		var item_name	=	document.createElement("input");
-		item_name.type	=	"hidden";
-		item_name.name	=	"item_name"
-		item_name.value	=	ppitem_name;
-		tempForm.appendChild(item_name);
-
-		var no_note	=	document.createElement("input");
-		no_note.type	=	"hidden";
-		no_note.name	=	"no_note"
-		no_note.value	=	"0";
-		tempForm.appendChild(no_note);
-
-		var amount	=	document.createElement("input");
-		amount.type	=	"hidden";
-		amount.name	=	"amount"
-		amount.value	=	donatrNo;
-		tempForm.appendChild(amount);
-
-		var currency_code	=	document.createElement("input");
-		currency_code.type	=	"hidden";
-		currency_code.name	=	"currency_code"
-		currency_code.value	=	"USD";
-		tempForm.appendChild(currency_code);
-
-		var bn	=	document.createElement("input");
-		bn.type	=	"hidden";
-		bn.name	=	"bn"
-		bn.value	=	"PP-DonationsBF:btn_donate_SM.gif:NonHostedGuest";
-		tempForm.appendChild(bn);
-
-		document.body.appendChild(tempForm);
-		tempForm.submit();
-		document.body.removeChild(tempForm);
-	}
+	
 
 	function showQR(QR) {
 		if (QR) {
@@ -138,17 +16,10 @@ jQuery(document).ready(function() {
 			MainBox.addClass('showQR');
 		});
 	}
-	//	Buttons
-	// $('body').on('click', '#PayPal', function() {
-	// 	ppBuy();	// 购买按钮适用于国内的 Paypal 账户
-	// 	// ppDonate();	// 捐赠按钮仅适合国外的 PayPal 账户
-	// });
 
 	$('#donateBox>li').click(function(event) {
 		var thisID	=	$(this).attr('id');
-		if (thisID === 'PayPal') {
-			ppBuy();
-		} else if (thisID === 'BTC') {
+		if (thisID === 'BTC') {
 			showQR(BTCQR);
 			new Clipboard('#BTCBn');
 		} else if (thisID === 'AliPay') {
