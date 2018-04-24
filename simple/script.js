f8727bcc498d2c8cc94f806df52b32ffd1358e90
Kaiyuan Xie <Kaiyuan@users.noreply.github.com>
2018-04-25 02:39:39 +0800

    微信二维码改为赞助码

--- simple/script.js
+++ simple/script.js
@@ -3,7 +3,7 @@ jQuery(document).ready(function() {
 	var MainBox	=	$('#MainBox');
 	var BTCQR	=	'images/BTCQR.png';	// 二维码路径
 	var AliPayQR	=	'images/AliPayQR.png';
-	var WeChanQR	=	'images/WeChanQR.png';
+	var WeChanQR	=	'images/WeChanSQ.png';
 
 	
 
