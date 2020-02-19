64c2ab2803a4818b95f40e06d606ca677f376bc5
Kaiyuan Xie <Kaiyuan@users.noreply.github.com>
2020-02-20 02:36:42 +0800

    补充提示，压缩图片。
    提醒 alipay_donate_link 的连接是移动设备跳转支付宝的连接，记得要修改。
    用 PP鸭压缩一下二维码。

--- drinks/script.js
+++ drinks/script.js
@@ -18,7 +18,7 @@ $('#github-box>a').href = GithubLink;
 var qrcodes = {
     'btc_donate'	:	'../simple/images/BTCQR.png',	// 二维码路径
     'alipay_donate'	:	'../simple/images/AliPayQR.png',	// 支付宝二维码
-    'alipay_donate_link'   :   'https://qr.alipay.com/3272611934645308',   // 支付宝二维码上的链接，必须换成自己的连接！！！
+    'alipay_donate_link'   :   'https://qr.alipay.com/3272611934645308',   // 支付宝二维码上的链接，必须换成自己的连接！！！手机点击会自动跳转到支付宝。
     'wechat_donate'	:	'../simple/images/WeChanSQ.png'
 };
 
